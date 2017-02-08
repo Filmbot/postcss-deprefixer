9999b436b3c8261c318993e06ecd99f3ad537591
sbussetti <steve.bussetti@gmail.com>
2017-02-08 09:27:30 -0500

    initial

---
+++ index.js
@@ -0,0 +1,14 @@
+var postcss = require('postcss');
+
+
+module.export = postcss.plugin( 'postcss-deprefixer', function( opts ) {
+  // this changes use of display: *box to use final spec display: flex
+  return function( css ) {
+    css.walkDecls( 'display', function( decl ) {
+      if (/^(\-[^\-]+\-)?box$/.test(decl.value)) {
+        decl.value = 'flex';
+      }
+    } );
+  }
+
+});
