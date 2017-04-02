6161f1ec2c769abf45eccc9d60cffdcd7967b67b
sbussetti <steve.bussetti@gmail.com>
2017-04-02 17:41:16 -0400

    allow for whitelisting

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 var postcss = require('postcss');
 
 
-module.exports = postcss.plugin( 'postcss-deprefixer', function( opts ) {
+module.exports = postcss.plugin( 'postcss-deprefixer', function( /*opts*/ ) {
   // this changes use of display: *box to use final spec display: flex
   return function( css ) {
-    css.walkDecls( 'display', function( decl ) {
-      if (/^(\-[^\-]+\-)?box$/.test(decl.value)) {
-        decl.value = 'flex';
+    css.walk( function( node ) {
+      if (node.type === 'decl' && node.prop === 'display' && /^(\-[^\-]+\-)?box$/.test(node.value)) {
+        var next = node.next();
+        if (!!next && next.type === 'comment' && next.text === 'deprefixer: off') {
+          return;
+        }
+        node.value = 'flex';
       }
     } );
   }
